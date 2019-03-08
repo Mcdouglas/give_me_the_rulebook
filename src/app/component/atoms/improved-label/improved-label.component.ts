b8ebcf512e2a0e5737ef7469984068256df624d9
Pierrick Knecht <pierrick-externe.knecht@enedis.fr>
2019-03-08 16:57:10 +0100

    [GMTR] Correction improved-label

--- src/app/component/atoms/improved-label/improved-label.component.ts
+++ src/app/component/atoms/improved-label/improved-label.component.ts
@@ -1,13 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'improved-label',
+    inputs: ['label', 'value'],
     templateUrl: './improved-label.component.html',
     styleUrls: ['./improved-label.component.scss']
 })
 export class ImprovedLabelComponent implements OnInit {
-    @Input() title: string;
-    @Input() value: string;
 
     constructor() {
     }
